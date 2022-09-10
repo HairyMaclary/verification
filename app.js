62a2e33e9fdce52f5975523eda55c7762804ced2
HairyMaclary <cliff.hamilton@outlook.com>
2022-09-10 15:01:50 +1200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '52.65.157.66';
-const port = 80;
+// const hostname = '52.65.157.66';
+// const port = 80;
 
-const server = http.createServer((req, res) => {
-  console.log('A request has arrived.')
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// const server = http.createServer((req, res) => {
+//   console.log('A request has arrived.')
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
 
-server.listen(port, "", () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, "", () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/',(req, res) => res.send('Hello World'));
+app.listen(PORT, () => console.log(`Server listening in port ${PORT}`))
