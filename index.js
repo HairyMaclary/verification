0c65d87434fa293732cc4dcd3ad85467206cd092
HairyMaclary <cliff.hamilton@outlook.com>
2022-09-15 09:47:27 +1200

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const indexFilePath = path.join(__dirname,'/index.html')
 
 console.log(indexFilePath);
 //app.get('/',(req, res) => res.send('Hello World'));
-app.get('/',(req, res) => res.sendFile(indexFilePath);
+app.get('/',(req, res) => res.sendFile(indexFilePath));
 app.listen(PORT, () => console.log(`Server listening in port ${PORT}`))
