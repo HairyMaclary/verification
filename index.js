7cc5f211a8bd09363824570113084c085c46f23b
HairyMaclary <cliff.hamilton@outlook.com>
2022-09-15 09:44:09 +1200

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-// const indexFilePath = path.join
+const indexFilePath = path.join(__dirname,'/index.html')
 
-
+console.log(indexFilePath);
 //app.get('/',(req, res) => res.send('Hello World'));
-app.get('/',(req, res) => res.sendFile('index.html', {root: __dirname });
+app.get('/',(req, res) => res.sendFile(indexFilePath);
 app.listen(PORT, () => console.log(`Server listening in port ${PORT}`))
